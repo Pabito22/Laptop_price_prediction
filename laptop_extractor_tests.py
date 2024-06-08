968e5d17c451ec10ce94519377213e2d9d99dc54
Pawel <witoszap@gmail.com>
2024-06-08 12:08:35 +0200

    Categories almost done, tests updeated for the new code.

--- laptop_extractor_tests.py
+++ laptop_extractor_tests.py
@@ -1,26 +1,26 @@
 import unittest
-from laptop_features_extractor import take_memory_size_and_type
+from laptop_features_extractor import take_size_type_fromMemory
 
 class MemoryTypeSizeTest(unittest.TestCase):
     """Tests for 'laptop_features_extractor.py'."""
     def test_one_word(self):
         """Are values:
         ['128GB SSD']
-         preprocessed right by take_memory_size_and_type
+         preprocessed right by take_size_type_fromMemory
         function?
         """
-        fromated_val = take_memory_size_and_type('128GB SSD')
+        fromated_val = take_size_type_fromMemory('128GB SSD')
         expected = [128.0, 'SSD']
         self.assertEqual(fromated_val, expected)
 
     def test_2_word(self):
         """Are values:
         ['128GB Flash Storag +  1TB HDD']
-         preprocessed right by take_memory_size_and_type
+         preprocessed right by take_size_type_fromMemory
         function?
         """
-        fromated_val = take_memory_size_and_type('128GB Flash Storag +  1TB HDD')
-        expected = [128.0, 'Other', 1024.0, 'HDD']
+        fromated_val = take_size_type_fromMemory('128GB Flash Storag +  1TB HDD')
+        expected = [128.0, 'Flash Storag', 1024.0, 'HDD']
         self.assertEqual(fromated_val, expected)
 
 if __name__ == '__main__':
